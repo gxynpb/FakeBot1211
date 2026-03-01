626bc296825447472543477fc962e8ac0749c580
gxynpb <167663187@qq.com>
2026-03-02 01:25:08 +0800

    Update plugin version and logging messages

--- src/main/java/me/fakebot/FakePlayerPlugin.java
+++ src/main/java/me/fakebot/FakePlayerPlugin.java
@@ -11,14 +11,20 @@ public class FakePlayerPlugin extends JavaPlugin {
         instance = this;
         manager = new FakePlayerManager();
         getCommand("fakeplayer").setExecutor(new FakePlayerCommand());
-        getLogger().info("§aFakeBot 1.21.10 加载成功 | 指令 /bot");
+        getLogger().info("§aFakeBot 1.21.11 加载成功 | 指令 /bot");
     }
 
     @Override
     public void onDisable() {
         manager.removeAll();
+        getLogger().info("§cFakeBot 已卸载，所有假人已移除");
     }
 
-    public static FakePlayerPlugin getInstance() { return instance; }
-    public FakePlayerManager getManager() { return manager; }
+    public static FakePlayerPlugin getInstance() {
+        return instance;
+    }
+
+    public FakePlayerManager getManager() {
+        return manager;
+    }
 }
