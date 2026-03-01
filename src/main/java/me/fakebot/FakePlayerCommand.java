0fcd2341bc63a4fcc121a9cf65d03e99f441da8e
gxynpb <167663187@qq.com>
2026-03-02 01:25:40 +0800

    Refactor command handling in FakePlayerCommand

--- src/main/java/me/fakebot/FakePlayerCommand.java
+++ src/main/java/me/fakebot/FakePlayerCommand.java
@@ -1,6 +1,8 @@
 package me.fakebot;
 
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class FakePlayerCommand implements CommandExecutor {
@@ -8,18 +10,28 @@ public class FakePlayerCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
-        if(!(s instanceof Player p) || !p.isOp()) {
+        if (!(s instanceof Player p) || !p.isOp()) {
             s.sendMessage("§c无权限");
             return true;
         }
-        if(a.length == 0) {
+        if (a.length == 0) {
             p.sendMessage("§e/bot create 名字 | remove 名字 | list");
             return true;
         }
         switch(a[0].toLowerCase()) {
-            case "create" -> { if(a.length>1) { m.createFakePlayer(a[1], p.getLocation()); p.sendMessage("§a创建成功"); } }
-            case "remove" -> { if(a.length>1) { m.removeFakePlayer(a[1]); p.sendMessage("§c删除成功"); } }
-            case "list" -> p.sendMessage("§a假人: "+m.getList());
+            case "create" -> {
+                if (a.length > 1) {
+                    m.createFakePlayer(a[1], p.getLocation());
+                    p.sendMessage("§a创建成功");
+                }
+            }
+            case "remove" -> {
+                if (a.length > 1) {
+                    m.removeFakePlayer(a[1]);
+                    p.sendMessage("§c删除成功");
+                }
+            }
+            case "list" -> p.sendMessage("§a假人: " + m.getList());
         }
         return true;
     }
